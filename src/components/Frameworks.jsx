035bc9832293d0f03d6f629fafd03ad9efd2c030
theweirdguyyy <theweirdguyyyy@gmail.com>
2026-02-12 13:44:03 +0600

    ..

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -21,7 +21,7 @@ export function Frameworks() {
     // "nextjs",
   ];
   return (
-    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+    <div className="relative flex h-60 w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
           <Icon key={index} src={`assets/logos/${skill}.svg`} />
